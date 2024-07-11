743e3974e24a9d9254d2f00cf41811d6c4634cc4
Cmoon <movers.garcia@gmail.com>
2024-07-11 14:46:18 +0800

    adding responsiveness

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 $(document).ready(() => {
-	const navTabLink = $("ul a");
-	const showImageProduct = $("main section");
-	const buttonSlider = [$(".buttonSlider_first"), $(".buttonSlider_second")];
-	let defaultIndex = 0;
-	const infoCircleToggle = $(".infoCircle div li");
+	const navTabLink = $(".navLinks a");
 
 	navTabLink.on("click", (event) => {
 		event.preventDefault();
 		navTabLink.removeClass("activeNavTab");
 		$(event.currentTarget).addClass("activeNavTab");
 	});
+
+	const menuBar = $(".menuButton");
+	const navLinks = $(".navLinks");
+	const closeMenu = $(".closeMenu");
+
+	menuBar.hide();
+	closeMenu.hide();
+
+	const openMenu = () => {
+		navLinks.addClass("sideMenu");
+		menuBar.hide();
+		closeMenu.show();
+	};
+
+	const closeMenuFunction = () => {
+		navLinks.removeClass("sideMenu");
+		closeMenu.hide();
+		menuBar.show();
+	};
+
+	const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+	const handleMediaChange = (e) => {
+		if (!e.matches) {
+			menuBar.show();
+			menuBar.on("click", openMenu);
+			closeMenu.on("click", closeMenuFunction);
+		} else {
+			navLinks.removeClass("sideMenu");
+			menuBar.hide();
+			closeMenu.hide();
+		}
+	};
+
+	mediaQuery.addEventListener("change", handleMediaChange);
+	handleMediaChange(mediaQuery);
+
+	const showImageProduct = $("main section");
+	const buttonSlider = [$(".buttonSlider_first"), $(".buttonSlider_second")];
+	let defaultIndex = 0;
+	const infoCircleToggle = $(".infoCircle div li");
 
 	const slideImage = (newIndex) => {
 		if (defaultIndex == newIndex) return;
@@ -41,6 +78,8 @@ $(document).ready(() => {
 
 	const imgContainer = $(".imgContainer");
 	const buttonCloseSlider = $(".buttonCloseSlider");
+
+	buttonCloseSlider.hide();
 
 	const closeSlider = () => {
 		imgContainer.removeClass("imgContainerShow").addClass("imgContainerActive");
